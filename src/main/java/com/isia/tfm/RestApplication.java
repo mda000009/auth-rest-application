5f3688482dba794b81efcb9d52852c18f4656c27
mda000009 <0610809824@uma.es>
2025-04-27 16:50:18 +0200

    development

---
+++ src/main/java/com/isia/tfm/RestApplication.java
@@ -0,0 +1,15 @@
+package com.isia.tfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
--- src/main/java/com/isia/tfm/config/SecurityConfig.java
+++ src/main/java/com/isia/tfm/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.isia.tfm.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +23,16 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(
+                new javax.crypto.spec.SecretKeySpec(
+                        secretKey.getBytes(),
+                        "HmacSHA256"
+                )
+        ).build();
     }
 
 }
