c996ea494c047eb1372b0eb4a95d7459b9e1ce1e
mda000009 <0610809824@uma.es>
2025-04-27 13:46:52 +0200

    add config

---
+++ src/main/java/com/isia/tfm/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.isia.tfm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+
+}
